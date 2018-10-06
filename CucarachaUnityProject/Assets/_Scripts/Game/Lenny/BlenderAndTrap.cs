dfc7fb89d7ba895f369cfca664bf02c3d82d8013
Lebenche <lenbencherif@gmail.com>
2018-10-06 12:02:12 +0200

    Add script for Food/light & Blender/trap

---
+++ CucarachaUnityProject/Assets/_Scripts/Game/Lenny/BlenderAndTrap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlenderAndTrap : MonoBehaviour {
+  public bool isBlender;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  private void OnTriggerEnter(Collider collision)
+  {
+    if (collision.gameObject.tag == "Cucaracha" && isBlender == true) {
+      Debug.Log("Cucaracha is dead with Blender");
+    }
+    else if (collision.gameObject.tag == "Cucaracha" && isBlender == false) {
+      Debug.Log("Cucaracha is dead with trap");
+    }
+  }
+}
---
+++ CucarachaUnityProject/Assets/_Scripts/Game/Lenny/FoodAndLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodAndLight : MonoBehaviour {
+
+  public bool isFood;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+   
+	}
+
+  private void OnTriggerEnter(Collider collision)
+  {
+    if (collision.gameObject.tag == "Cucaracha" && isFood == true) {
+      Debug.Log("Cucaracha follow the food");
+    }
+    else if (collision.gameObject.tag == "Cucaracha" && isFood == false) {
+      Debug.Log("Cucaracha flees the light");
+    }
+  }
+}
