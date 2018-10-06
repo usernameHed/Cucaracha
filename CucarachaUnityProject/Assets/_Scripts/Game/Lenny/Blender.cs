8e21fb4b019c58a05e9082cf263ec9bc05d0a632
Lebenche <lenbencherif@gmail.com>
2018-10-06 19:55:43 +0200

    Add Life gauge proto

--- CucarachaUnityProject/Assets/_Scripts/Game/Lenny/Blender.cs
+++ CucarachaUnityProject/Assets/_Scripts/Game/Lenny/Blender.cs
@@ -1,23 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Blender : MonoBehaviour {
+public class Blender : MonoBehaviour
+{
+  int juiceQuantity = 0;
+  public GameObject Slider;
+  private float maximumScore;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  // Use this for initialization
+  void Start () {
+    changeSlider();
+    maximumScore = Slider.GetComponent<Slider>().maxValue;
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
   private void OnTriggerEnter(Collider collision)
   {
     if (collision.gameObject.CompareTag(GameData.Layers.Cucaracha.ToString())) {
+      changeSlider();
+
+        juiceQuantity++;
+        Debug.Log("Juice quantity : " + juiceQuantity);
+
+      
+
+      if (juiceQuantity > maximumScore) {
+        Debug.Log("It's over 9000 ! ");
+
+      }
+
+
       Debug.Log("Cucaracha is dead with Blender");
     }
     
+  }
+
+  void changeSlider()
+  {
+    Slider.GetComponent<Slider>().value = juiceQuantity;
+
   }
 }
