58f0a8cf3acad0e4d80980aa7006bf3ef1c9e941
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-10-06 22:17:57 +0200

    lifeFood ?

--- CucarachaUnityProject/Assets/_Scripts/Game/Lenny/LifeFood.cs
+++ CucarachaUnityProject/Assets/_Scripts/Game/Lenny/LifeFood.cs
@@ -2,46 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LifeFood : MonoBehaviour, IKillable {
-  int lifeFood = 100;
-  float timerFood;
-	// Use this for initialization
-	void Start () {
-    timerFood = Time.fixedTime;
+public class LifeFood : MonoBehaviour, IKillable
+{
+    int lifeFood = 100;
+    float timerFood;
 
-  }
+    [SerializeField]
+    private Food food;
 
-  // Update is called once per frame
-  void Update () {
-   // Debug.Log("Time delta time : " + Time.deltaTime);
-
-  }
-
-  private void OnTriggerStay(Collider collision)
-  {
-
-    if (collision.gameObject.CompareTag(GameData.Layers.Cucaracha.ToString())) {
-
-      //GameData
-      if ( Time.fixedTime > timerFood + 0.5f ) {
-        lifeFood--;
-        timerFood = Time.fixedTime + 0.5f;
-        Debug.Log("life food : " + lifeFood);
-
-      }
-
-
-      if (lifeFood <= 0) {
-        lifeFood = 0;
-        Kill();
-
-      }
+    // Use this for initialization
+    void Start ()
+    {
+        timerFood = Time.fixedTime;
     }
 
-  }
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.CompareTag(GameData.Layers.Cucaracha.ToString()))
+        {
+            //GameData
+            if ( Time.fixedTime > timerFood + 0.5f )
+            {
+                lifeFood--;
+                timerFood = Time.fixedTime + 0.5f;
+                Debug.Log("life food : " + lifeFood);
+            }
 
-  public void Kill()
-  {
-    throw new System.NotImplementedException();
-  }
+            if (lifeFood <= 0)
+            {
+                lifeFood = 0;
+                Kill();
+            }
+        }
+
+    }
+
+    public void Kill()
+    {
+        food.Kill();
+    }
 }
