4f616979f091d00f1b5d1f95aa4f8cfbac8a94f2
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-10-07 14:54:21 +0200

    added percentage on spawn

--- CucarachaUnityProject/Assets/_Scripts/Game/SpawnPoint.cs
+++ CucarachaUnityProject/Assets/_Scripts/Game/SpawnPoint.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
+    [Serializable]
+    public struct SpawnInfo
+    {
+        public Transform spawn;
+        public float percentage;
+    }
+
     [SerializeField]
     private float rangeSpawn = 5f;
 
     [SerializeField]
-    private List<Transform> spawnPoint = new List<Transform>();
+    private List<SpawnInfo> spawnPoint = new List<SpawnInfo>();
 
     private void Start()
     {
@@ -25,14 +33,33 @@ public class SpawnPoint : MonoBehaviour
     /// called for getting a player position
     /// </summary>
     /// <returns></returns>
-    public Vector3 GetPosSpawn()
+    private Vector3 GetPosSpawn()
     {
         Pointf point = new Pointf(0.0f, 0.0f);
 
-        int randomList = UnityEngine.Random.Range(0, spawnPoint.Count);
-        point.x = spawnPoint[randomList].position.x + (UnityEngine.Random.Range(0.0f, rangeSpawn) * ExtRandom.RandomNegative());
-        point.y = spawnPoint[randomList].position.z + (UnityEngine.Random.Range(0.0f, rangeSpawn) * ExtRandom.RandomNegative());
+        bool isInside = false;
+        int randomList = UnityEngine.Random.Range(0, 100);
+        float actualPertentage = 0;
+        for (int i = 0; i < spawnPoint.Count; i++)
+        {
+            if (randomList >= actualPertentage && randomList <= actualPertentage + spawnPoint[i].percentage)
+            {
+                //ici ok
+                point.x = spawnPoint[i].spawn.position.x + (UnityEngine.Random.Range(0.0f, rangeSpawn) * ExtRandom.RandomNegative());
+                point.y = spawnPoint[i].spawn.position.z + (UnityEngine.Random.Range(0.0f, rangeSpawn) * ExtRandom.RandomNegative());
+                isInside = true;
+                break;
+            }
+            actualPertentage += spawnPoint[i].percentage;
+        }
 
+        if (!isInside)
+        {
+            point.x = spawnPoint[0].spawn.position.x + (UnityEngine.Random.Range(0.0f, rangeSpawn) * ExtRandom.RandomNegative());
+            point.y = spawnPoint[0].spawn.position.z + (UnityEngine.Random.Range(0.0f, rangeSpawn) * ExtRandom.RandomNegative());
+        }
+
+        
         return (new Vector3(point.x, 0, point.y));
     }
 
